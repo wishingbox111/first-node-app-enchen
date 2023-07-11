d74cdd24de02007752242a5863d133c3985eceb9
wishingbox111 <wishingbox111@gmail.com>
2023-07-11 21:33:55 +0800

    push

--- node.js
+++ node.js
@@ -4,30 +4,43 @@
 // to run 
 // npm start
 
-'use strict';
+//'use strict';
 
-const express = require('express');
+//const express = require('express');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-const OS = require('os');
-const ENV = 'DEV';
+//const PORT = 8080;
+//const HOST = '0.0.0.0';
+//const OS = require('os');
+//const ENV = 'DEV';
 
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from Node!';
-  res.send(msg);
-});
+//const app = express();
+//app.get('/', (req, res) => {
+//  res.statusCode = 200;
+//  const msg = 'Hello from Node!';
+//  res.send(msg);
+//});
 
-app.get('/test', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
-  res.send(msg);
-});
+//app.get('/test', (req, res) => {
+//  res.statusCode = 200;
+//  const msg = 'Hello from /test Node!';
+//  res.send(msg);
+//});
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+//app.listen(PORT, HOST);
+//console.log(`Running on http://${HOST}:${PORT}`);
+
+
+
+
+const http = require("http")
+const hostname = "localhost"
+const port = 3700;
+const server = http.createServer((req, res) => {
+ console.log(req.headers);
+ res.statusCode = 200;
+ res.end("<html><body><h1>Hello, World!</h1></body></html")
+})
+server.listen(port, hostname);
