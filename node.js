fdc48be4598d398007ff345aec1d18521c9e2783
wishingbox111 <121477591+wishingbox111@users.noreply.github.com>
2023-07-13 20:15:57 +0800

    Update node.js

--- node.js
+++ node.js
@@ -4,50 +4,50 @@
 // to run 
 // npm start
 
-//'use strict';
+'use strict';
 
-//const express = require('express');
+const express = require('express');
 
 // Constants
-//const PORT = 8080;
-//const HOST = '0.0.0.0';
-//const OS = require('os');
-//const ENV = 'DEV';
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const OS = require('os');
+const ENV = 'DEV';
 
 
 // App
-//const app = express();
-//app.get('/', (req, res) => {
-//  res.statusCode = 200;
-//  const msg = 'Hello from Node!';
-//  res.send(msg);
-//});
+const app = express();
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from Node!';
+  res.send(msg);
+});
 
-//app.get('/test', (req, res) => {
-//  res.statusCode = 200;
-//  const msg = 'Hello from /test Node!';
-//  res.send(msg);
-//});
+app.get('/test', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from /test Node!';
+  res.send(msg);
+});
 
-//app.listen(PORT, HOST);
-//console.log(`Running on http://${HOST}:${PORT}`);
-
-
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 
 
-const http = require("http")
-const hostname = "localhost"
-const port = 3700;
-const server = http.createServer((req, res) => {
- console.log(req.headers);
- res.statusCode = 200;
- res.end("<html><body><h1>Hello, World!</h1></body></html")
-})
-"scripts" (
-    "start"= "node node.js",
-    "test"= "echo \"Error: no test specified\" && exit 1"
-);
-server.listen(port, hostname);
+
+//below code can also run and replace the above but it is not defined as "express"
+//const http = require("http")
+//const hostname = "localhost"
+//const port = 3700;
+//const server = http.createServer((req, res) => {
+// console.log(req.headers);
+// res.statusCode = 200;
+// res.end("<html><body><h1>Hello, World!</h1></body></html")
+//})
+//"scripts" (
+//    "start"= "node node.js",
+//    "test"= "echo \"Error: no test specified\" && exit 1"
+//);
+//server.listen(port, hostname);
 
 
 //still has problem with npm start. But npm install express works. Tired delete folder and clone again but didn't work toooo.
